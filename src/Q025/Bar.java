aa3b51cbe62c5c8a13762b36ceb3616cde2dc241
Ozlem Citci <ozlmctc@gmail.com>
2021-03-30 14:20:56 -0500

    update

--- src/Q025/Bar.java
+++ src/Q025/Bar.java
@@ -1,5 +1,20 @@
 package Q025;
 
-public class Bar {
+public class Bar extends Foo {
 
 }
+/*
+Which two statements are true if the method is added to Bar? (Choose two.)
+A. public Collection<String> foo(Collection<String> arg) { ... } overrides Foo.foo. B. public <T> Collection<T> foo(Stream<T> arg) { ... } overloads Foo.foo.
+C. public <T> List<T> foo(Collection<T> arg) { ... } overrides Foo.foo.
+D. public <T> Collection<T> foo(Collection<T> arg) { ... } overloads Foo.foo.
+E. public <T> Collection<T> bar(Collection<T> arg) { ... } overloads Foo.foo. F. public <T> Iterable<T> foo(Collection<T> arg) { ... } overrides Foo.foo.
+Question: 26
+Given the code fragment:
+What is the result?
+A. ab cd ef
+B. An ArrayIndexOutOfBoundsException is thrown at runtime. C. The compilation fails.
+D. abc def
+E. ad be cf*/
+
+//Cevabini bilmiyorum
