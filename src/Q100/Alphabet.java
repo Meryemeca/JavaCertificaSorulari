e23f1d89a8a1a8193f5319dba793d4a8a3767660
Meryemeca <meryem.kus@gmail.com>
2021-04-03 23:17:39 -0500

    Questions Q96-100 and 105-115 added.

---
+++ src/Q100/Alphabet.java
@@ -0,0 +1,29 @@
+package Q100;
+
+
+//ANSWER C
+
+/*
+ * 
+ * Examine this code: 
+ * System.out.println(Alphabet.getFirstLetter());
+
+What code should be written at line 3 to make this code print A?
+
+A. final String getFirstLetter() { return A.toString(); }
+B. static String getFirstLetter() { return Alphabet.values()[1].toString(); } 
+C. static String getFirstLetter() { return A.toString(); }
+D. String getFirstLetter() { return A.toString(); }
+ * 
+ */
+
+
+public enum Alphabet {
+	
+	A, B, C
+	
+	
+	
+
+
+}
